692e6cfb4f1a846a9b4ed2b64ed0d3bf6c4f7f4c
ismuchenzhou <ismuchenzhou@gmail.com>
2019-05-14 17:24:18 +0800

    p4

---
+++ p4/src/main/java/webadv/s99201105/p4/HomeController.java
@@ -0,0 +1,26 @@
+package webadv.s99201105.p4;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	// inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+	
+    private List<String> tasks = Arrays.asList("Git", "Maven", "Gradle", "Spring", "Bootstrap", "jQuery", "MyBatis","Redis");
+    
+    @GetMapping("/")
+	public String index(Model model) {
+    	model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+		return "welcome";		
+	}
+}
---
+++ p4/src/main/java/webadv/s99201105/p4/P4Application.java
@@ -0,0 +1,13 @@
+package webadv.s99201105.p4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P4Application.class, args);
+	}
+
+}
